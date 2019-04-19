e3c5e16551f031d1cccdb3e16f500a437f2d1afe
Charles Jackson <charles@Charles-PC.home>
2019-04-19 01:01:58 -0500

    optimized output to show acurate percentage of success

--- UMLER/TestRunner.java
+++ UMLER/TestRunner.java
@@ -4,13 +4,17 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
 public class TestRunner {
-   public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(TestUMLER.class);
-		
-      for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
-      }
-		
-      System.out.println((result.wasSuccessful()? "100% pass":"failed"));
-   }
+    public static void main(String[] args) {
+	Result result = JUnitCore.runClasses(TestUMLER.class);
+
+	for (Failure failure : result.getFailures()) {
+	    System.out.println(failure.toString());
+	}
+	double percent= result.getFailureCount()/(double)result.getRunCount();
+	percent =1-percent;
+	percent *=100;
+	
+	System.out.println((int)percent+"% Pass");
+
+    }
 }  	
